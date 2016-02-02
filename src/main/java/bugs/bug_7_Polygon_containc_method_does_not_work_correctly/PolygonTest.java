79103c27f4e89d6b93e2f6ce21182f960fac5dbf
Dmitriy Kapelushniy <test_git@mail.ru>
2016-02-02 15:43:57 +0200

    Seventh bug i dont know how to fix))

--- src/main/java/bugs/bug_7_Polygon_containc_method_does_not_work_correctly/PolygonTest.java
+++ src/main/java/bugs/bug_7_Polygon_containc_method_does_not_work_correctly/PolygonTest.java
@@ -1,14 +1,30 @@
 package bugs.bug_7_Polygon_containc_method_does_not_work_correctly;
 
 import java.awt.*;
+import java.util.Arrays;
 
-public class PolygonTest {
+public class PolygonTest extends Polygon {
     public static void main(String[] args) {
         int[] xPoints = {-2, 2, -2, 2};
         int[] yPoints = {2, 2, -2, -2};
 
-        Polygon polygon = new Polygon(xPoints, yPoints, xPoints.length);
+        Polygon polygon = new PolygonTest(xPoints, yPoints, xPoints.length);
+        System.out.println(Arrays.toString(xPoints) + Arrays.toString(yPoints) + " " + xPoints.length);
         boolean checkPoint = polygon.contains(1, 1);
+
+
         System.out.print(checkPoint);//why false?) -> should be true))
+    }
+
+    PolygonTest(int xpoints[], int ypoints[], int npoints){
+        super(xpoints, ypoints, npoints);
+    }
+    @Override
+    public boolean contains(double x, double y){
+        if( x==y ){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
